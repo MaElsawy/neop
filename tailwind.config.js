cbc5bd2c8a43c141c0c3e05e393100ee744851c9
Your Name <your.email@example.com>
2024-11-04 14:50:35 +0200

    edit

---
+++ tailwind.config.js
@@ -0,0 +1,55 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      colors: {
+        'primary': '#004876',
+        'secondary': '#3B2F2F',
+        'tertiary': '#292625',
+        'quaternary': '#6F4336',
+        'quinary': '#1E71A6',
+        'senary': '#8B8BA5',
+      },
+      screens: {
+        'xs': '400px',
+        // => @media (min-width: 400px) { ... }
+        'sm': '640px',
+        // => @media (min-width: 640px) { ... }
+        'md': '768px',
+        // => @media (min-width: 768px) { ... }
+        'lg': '1024px',
+        // => @media (min-width: 1024px) { ... }
+        'xl': '1280px',
+        // => @media (min-width: 1280px) { ... }
+        '2xl': '1536px',
+        // => @media (min-width: 1536px) { ... }
+      },
+      backgroundImage: {
+        'cube': "url('/assets/imgs/cube.svg'), url('/src/assets/imgs/cube.svg')",
+        'back': "url('/assets/imgs/back.svg'), url('/src/assets/imgs/back.svg')",
+        'blob':"url('/assets/imgs/blob.svg'), url('/src/assets/imgs/blob.svg')",
+      },
+      fontFamily: {
+        'primary': ['Quicksand'],
+        'secondary': ['Cormorant Infant'],
+        'tertiary': ['Puritan'],
+      },
+      keyframes:{
+        'slide-left-right':{
+          '0%,100%':{transform: 'translateX(0)'},
+          '50%':{transform: 'translateX(10px)'}
+        },
+      },
+      animation:{
+        'slide-left-right': 'slide-left-right 3s ease-in-out infinite',
+      },
+      container: {
+        center: true,
+        padding: "1rem",
+      },
+    },
+  },
+  plugins: [],
+}
+

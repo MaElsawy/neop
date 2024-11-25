baad7cb24e976f1f355042bfb3b174992fbb1252
Your Name <your.email@example.com>
2024-11-25 16:59:51 +0200

    edits

--- tailwind.config.js
+++ tailwind.config.js
@@ -61,6 +61,18 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    function ({ addUtilities }) {
+      addUtilities({
+        '.no-scrollbar': {
+          '-ms-overflow-style': 'none',
+          'scrollbar-width': 'none',
+        },
+        '.no-scrollbar::-webkit-scrollbar': {
+          display: 'none',
+        },
+      });
+    },
+  ],
 }
 
